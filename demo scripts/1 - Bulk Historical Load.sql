8c91812df13cfa1ea255e9a68c8f70bec3da81be
Mike Wies <mike.wies@snowflake.com>
2021-12-06 21:37:43 -0500

    cleaned up ingestion code. No more manual coding pipe DDL. File '2.1 - Data Marts' and file '4 - Automated Pipeline' should be merged. Whole solution should be migrated to use a single payload variant LANDING table, with a task to SELECT FROM LANDING and INSERT INTO RAW... This way, the task could bake in a table migration call before each task execution. Currently, there is a paradox where the file schema won't be picked up until a file with a new field is landed, and once it is landed it will be ingested before the ftable is updated

--- demo scripts/1 - Bulk Historical Load.sql
+++ demo scripts/1 - Bulk Historical Load.sql
@@ -2,113 +2,46 @@
 USE ROLE GUIDEWIRE_ADMIN;
 USE WAREHOUSE GW_CDA_LOAD_WH;
 
--- WRITE AN EXTERNAL TABLE DEFN (This will simplify the load query later)
-  CREATE OR REPLACE EXTERNAL TABLE GUIDEWIRE_CDA.LANDING.bc_chargept_nm_l10n_ext
-    ( filename string AS METADATA$FILENAME)
-    LOCATION = @GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire/bc_chargept_nm_l10n/
-    REFRESH_ON_CREATE =  TRUE
-    AUTO_REFRESH = TRUE
-    PATTERN = '.*.parquet'
-    FILE_FORMAT = (type = parquet);
-    
-SELECT * FROM GUIDEWIRE_CDA.LANDING.bc_chargept_nm_l10n_ext;
-
 -- Inspect the schema of the external table using schema inference
   select *
     from table(
       INFER_SCHEMA(
-      LOCATION=>'@GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire/bc_chargept_nm_l10n/'
+      LOCATION=>'@GUIDEWIRE_CDA.RAW.gw_bc_chargept_nm_l10n_extstg/'
       , FILE_FORMAT=>'GUIDEWIRE_CDA.PUBLIC.FILE_FORMAT_PARQUET_DEFAULT'
         )
       )order by 1;
 
 -- CREATE A TARGET TABLE TO LOAD THE RAW CDC DATA IN A STRUCTURED FORMAT 
-  call guidewire_cda.public.migrate_s3_table('GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire','bc_chargept_nm_l10n/','Parquet','GUIDEWIRE_CDA','RAW','BC_CHARGEPT_NM_L10N');
--- Create an external table to facilitate the load from external 
-  call guidewire_cda.public.et_from_s3('GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire','bc_chargept_nm_l10n/','Parquet','GUIDEWIRE_CDA','RAW','BC_CHARGEPT_NM_L10N');
--- LOAD THE RAW CDC DATA IN A STRUCTURED FORMAT 
-  INSERT INTO GUIDEWIRE_CDA.RAW.BC_CHARGEPT_NM_L10N(
-    BEANVERSION
-    ,GWCBI___CONNECTOR_TS_MS
-    ,GWCBI___LSN 
-    ,GWCBI___OPERATION 
-    ,GWCBI___PAYLOAD_TS_MS 
-    ,GWCBI___SEQVAL_HEX
-    ,GWCBI___TX_ID
-    ,ID
-    ,LANGUAGE
-    ,OWNER
-    ,PUBLICID
-    ,VALUE
-  ) SELECT
-    BEANVERSION
-    ,GWCBI___CONNECTOR_TS_MS
-    ,GWCBI___LSN 
-    ,GWCBI___OPERATION 
-    ,GWCBI___PAYLOAD_TS_MS 
-    ,GWCBI___SEQVAL_HEX
-    ,GWCBI___TX_ID
-    ,ID
-    ,LANGUAGE
-    ,OWNER
-    ,PUBLICID
-    ,VALUE::STRING
-  FROM GUIDEWIRE_CDA.RAW.BC_CHARGEPT_NM_L10N_ET;
+  call guidewire_cda.public.migrate_s3_table('GUIDEWIRE_CDA.RAW.gw_bc_chargept_nm_l10n_extstg', '', 'parquet', 'GUIDEWIRE_CDA','RAW','BC_CHARGEPT_NM_L10N');
 
+-- SIMPLE SELECT and INSERT TO LOAD (Can be built in to a scheduled task or executed in a pipeline)
+  call guidewire_cda.public.bulk_load('GUIDEWIRE_CDA.RAW.gw_bc_chargept_nm_l10n_extstg', '', 'parquet', 'GUIDEWIRE_CDA','RAW','BC_CHARGEPT_NM_L10N');
 -- Preview Raw table in Object Browser
+
+
+-- Generally we would want to create a pipe to handle incremental loads immediately after performing the initial bulk load...
+-- This is what that pipe would look like.
+
+CREATE OR REPLACE PIPE GUIDEWIRE_CDA.RAW.bc_chargept_nm_l10n
+AUTO_INGEST=TRUE 
+AWS_SNS_TOPIC = 'arn:aws:sns:us-west-2:484577546576:sfc-mwies-bc_chargept_nm_l10n'
+AS
+  copy into "GUIDEWIRE_CDA"."RAW"."BC_CHARGEPT_NM_L10N"
+  from '@GUIDEWIRE_CDA.RAW.gw_bc_chargept_nm_l10n_extstg'
+      FILE_FORMAT = (TYPE = 'PARQUET'),
+      MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
+      PATTERN = '.*.parquet'
+;
+
 
 /* ===========================================================
 /* =========   REPEAT FOR TACCOUNTPATTERN TABLE   ========= */
 
-
-
 -- CREATE A TARGET TABLE TO LOAD THE RAW CDC DATA IN A STRUCTURED FORMAT 
-  call guidewire_cda.public.migrate_s3_table('GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire','bc_taccountpattern/','Parquet','GUIDEWIRE_CDA','RAW','BC_TACCOUNTPATTERN');
--- Create an external table to facilitate the load from external 
-  call guidewire_cda.public.et_from_s3('GUIDEWIRE_CDA.LANDING.s3_extstg_guidewire','bc_taccountpattern/','Parquet','GUIDEWIRE_CDA','RAW','BC_TACCOUNTPATTERN');
--- LOAD THE RAW CDC DATA IN A STRUCTURED FORMAT 
-  INSERT INTO GUIDEWIRE_CDA.RAW.BC_TACCOUNTPATTERN(
-    BEANVERSION
-    ,CHARGEPATTERNID
-    ,CREATETIME
-    ,CREATEUSERID
-    ,GWCBI___CONNECTOR_TS_MS
-    ,GWCBI___LSN 
-    ,GWCBI___OPERATION 
-    ,GWCBI___PAYLOAD_TS_MS 
-    ,GWCBI___SEQVAL_HEX
-    ,GWCBI___TX_ID
-    ,ID
-    ,PUBLICID
-    ,RETIRED
-    ,SUBTYPE
-    ,SUFFIX
-    ,TACCOUNTLAZYLOADED
-    ,TACCOUNTNAME
-    ,TACCOUNTOWNERPATTERNID
-    ,TACCOUNTTYPE
-    ,UPDATETIME
-    ,UPDATEUSERID
-  ) SELECT
-    BEANVERSION
-    ,CHARGEPATTERNID
-    ,CREATETIME
-    ,CREATEUSERID
-    ,GWCBI___CONNECTOR_TS_MS
-    ,GWCBI___LSN 
-    ,GWCBI___OPERATION 
-    ,GWCBI___PAYLOAD_TS_MS 
-    ,GWCBI___SEQVAL_HEX
-    ,GWCBI___TX_ID
-    ,ID
-    ,PUBLICID
-    ,RETIRED
-    ,SUBTYPE
-    ,SUFFIX
-    ,TACCOUNTLAZYLOADED
-    ,TACCOUNTNAME
-    ,TACCOUNTOWNERPATTERNID
-    ,TACCOUNTTYPE
-    ,UPDATETIME
-    ,UPDATEUSERID
-  FROM GUIDEWIRE_CDA.RAW.BC_TACCOUNTPATTERN_ET;
+  call guidewire_cda.public.migrate_s3_table('GUIDEWIRE_CDA.RAW.gw_bc_taccountpattern_extstg', '', 'parquet', 'GUIDEWIRE_CDA','RAW','BC_TACCOUNTPATTERN');
+
+-- SIMPLE SELECT and INSERT TO LOAD (Can be built in to a scheduled task or executed in a pipeline)
+  call guidewire_cda.public.bulk_load('GUIDEWIRE_CDA.RAW.gw_bc_taccountpattern_extstg', '', 'parquet', 'GUIDEWIRE_CDA','RAW','BC_TACCOUNTPATTERN');
+-- Preview Raw table in Object Browser
+
+SELECT 'GW 1 Complete.';
